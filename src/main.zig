2a541e4f8292e8aa308f4ccb6dfb5aada6ddd8c6
Zachary Juang <zachary822@me.com>
2024-11-21 02:29:41 -0500

    scuffed search

--- src/main.zig
+++ src/main.zig
@@ -7,9 +7,12 @@ pub fn main() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
     var mmdb: c.MMDB_s = undefined;
 
-    var status = c.MMDB_open("GeoLite2-ASN.mmdb", c.MMDB_MODE_MMAP, &mmdb);
+    var status = c.MMDB_open(args[1], c.MMDB_MODE_MMAP, &mmdb);
     defer c.MMDB_close(&mmdb);
 
     if (status != c.MMDB_SUCCESS) {
@@ -17,7 +20,8 @@ pub fn main() !u8 {
         return 1;
     }
 
-    const ip: [:0]const u8 = "8.8.8.8";
+    const ip = try allocator.dupeZ(u8, args[2]);
+    defer allocator.free(ip);
 
     var gai_error: c_int = undefined;
 
@@ -38,18 +42,28 @@ pub fn main() !u8 {
         return 0;
     }
 
+    std.debug.print("netmask: {d}\n", .{result.netmask - 96});
+
     var entry_data: c.MMDB_entry_data_s = undefined;
     status = c.MMDB_get_value(&result.entry, &entry_data, "autonomous_system_organization");
 
     if (status == c.MMDB_SUCCESS and entry_data.has_data) {
-        const str = try allocator.allocSentinel(u8, entry_data.data_size, 0);
-        defer allocator.free(str);
+        std.debug.print("type: {d}\n", .{entry_data.type});
 
-        for (0..entry_data.data_size) |i| {
-            str[i] = entry_data.unnamed_0.utf8_string[i];
+        switch (entry_data.type) {
+            c.MMDB_DATA_TYPE_UTF8_STRING => {
+                const str = try allocator.allocSentinel(u8, entry_data.data_size, 0);
+                defer allocator.free(str);
+
+                for (0..entry_data.data_size) |i| {
+                    str[i] = entry_data.unnamed_0.utf8_string[i];
+                }
+
+                std.debug.print("AS Organization: {s}\n", .{str});
+            },
+            else => {},
         }
 
-        std.debug.print("AS Organization: {s}\n", .{str});
         return 0;
     }
 
